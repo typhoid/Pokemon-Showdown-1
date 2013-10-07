ca58d438e80e5ee7f6f392a69e4f33aca704d034
E4Arsh <arshrulesrs@gmail.com>
2013-10-07 16:40:55 +0530

    Update commands.js

--- commands.js
+++ commands.js
@@ -1525,19 +1525,7 @@ hide: function(target, room, user) {
 		user.makeChallenge(targetUser, target);
 	},
 
-	away: 'blockchallenges',
-	idle: 'blockchallenges',
-	blockchallenges: function(target, room, user) {
-		user.blockChallenges = true;
-		this.sendReply('You are now blocking all incoming challenge requests.');
-	},
-
-	back: 'allowchallenges',
-	allowchallenges: function(target, room, user) {
-		user.blockChallenges = false;
-		this.sendReply('You are available for challenges from now on.');
-	},
-
+	
 	cchall: 'cancelChallenge',
 	cancelchallenge: function(target, room, user) {
 		user.cancelChallengeTo(target);
