f5062dd9c200010594f5c095468e861283ba3d85
E4Arsh <arshrulesrs@gmail.com>
2013-10-11 17:29:23 +0530

    Update tour.js

--- tour.js
+++ tour.js
@@ -304,7 +304,7 @@ exports.tour = function(t) {
 					if (tour[rid].size >= 16) {
 						tourMoney = 3;
 					}
-					if (tour[rid].size >= 8 && tour[rid].size < 7) {
+					if (tour[rid].size >= 8 && tour[rid].size < 4) {
 						tourMoney = 2;
 					}
 					if (tour[rid].size < 16 && tour[rid].size >= 4) {
