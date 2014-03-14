b4f43c7336d1dc452c76188ae07c7eac12c688a3
Elijah Bandi <imdatbulll@gmail.com>
2014-03-14 00:20:34 -0400

    o3o

--- stuff/chatbot/spamsystem.js
+++ stuff/chatbot/spamsystem.js
@@ -2,12 +2,13 @@ function CheckBannedSites(message) {
     var fs = require('fs');
     var data = fs.readFileSync('./stuff/chatbot/bannedsites.txt', 'utf8');
     var sites = data.split('\n');
-    for (var i = 0; i < sites.length; i++)
-    if (message.indexOf(sites[i]) > -1) {
-        return true;
-	} else {
-    return false;
-	}
+    for (var i = 0; i < sites.length; i++) {
+        if (message.indexOf(sites[i]) > -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
 function CheckBannedWords(message) {
@@ -15,12 +16,12 @@ function CheckBannedWords(message) {
     var data = fs.readFileSync('./stuff/chatbot/bannedwords.txt', 'utf8');
     var words = data.split('\n');
     for (var i = 0; i < words.length; i++) {
-    if (message.indexOf(words[i]) > -1) {
-        return true;
-    } else {
-    return false;
-	}
-	}
+        if (message.indexOf(words[i]) > -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 exports.canTalk = function (user, room, connection, message) {
     global.today = new Date();
@@ -32,18 +33,20 @@ exports.canTalk = function (user, room, connection, message) {
         user.numMessages = 0;
     }
     user.numMessages += 1;
-    if (CheckBannedSites(message) === true) {
-	user.mute(room.id, 60 * 60 * 1000);
-	    room.add('|html|<font color="#FF00BF"><i><b>' + bot.name + '</b> has muted ' + user.name + ' for an hour(bad site).</i></font>');
-        
+    var cb = CheckBannedSites(message);
+    var cm = CheckBannedWords(message) ;
+
+    if (cb === true) {
+        user.mute(room.id, 60 * 60 * 1000);
+        room.add('|html|<font color="#FF00BF"><i><b>' + bot.name + '</b> has muted ' + user.name + ' for an hour(bad site).</i></font>');
         return false;
     }
-    if (CheckBannedWords(message) === true) {
-	user.mute(room.id, 60 * 60 * 1000);
-	room.add('|html|<font color="#FF00BF"><i><b>' + bot.name + '</b> has muted ' + user.name + ' for an hour(spamword).</i></font>');
-        
+    if (cm === true) {
+        user.mute(room.id, 60 * 60 * 1000);
+        room.add('|html|<font color="#FF00BF"><i><b>' + bot.name + '</b> has muted ' + user.name + ' for an hour(spamword).</i></font>');
         return false;
     }
+
     if (user.numMessages == 15) {
         user.mute(room.id, 7 * 60 * 1000);
         room.add('|html|<font color="#FF00BF"><i><b>' + bot.name + '</b> has muted ' + user.name + ' for 7 minutes(flood).</i></font>');
@@ -59,73 +62,76 @@ exports.canTalk = function (user, room, connection, message) {
     var alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     for (var i = 0; i < alpha.length; i++) {
         if (message.toUpperCase().indexOf(alpha[i]) >= 0 && !user.can('broadcast')) {
-		user.warnCounters += 1;
-                room.add('|html|<font color="#FF00BF">' + user.name + ' was warned by ' + '<i><b>' + bot.name + '</b>(caps)</i></font>');
-                user.send('|c|~|/warn caps');
-}
-    }        if (user.warnCounters > 4) {
-                room.add('|html|<font color="#FF00BF">' + user.name + ' was muted by ' + '<i><b>' + bot.name + '</b>(more than 4 warnings)</i></font>');
-                user.mute(room.id, 60 * 60 * 1000, true);
-                return false;
-            }
-            if (message === message.toUpperCase() && message.length >= 11) {
-                room.add('|c|' + user.name + '|' + message);
-                user.warnCounters += 1;
-                room.add('|html|<font color="#FF00BF">' + user.name + ' was warned by ' + '<i><b>' + bot.name + '</b>(caps)</i></font>');
-                user.send('|c|~|/warn caps');
-                return false;
-            }
-        if (spamroom[user.userid]) {
-            Rooms.rooms.spamroom.add('|c|' + user.getIdentity() + '|' + message);
-            connection.sendTo(room, "|c|" + user.getIdentity() + "|" + message);
-            return false;
-        }
-        if (message.toLowerCase().indexOf(".psim") > -1) {
-            connection.sendTo(room, '|raw|<strong class=\"message-throttle-notice\">Advertising is not allowed please do not.</strong>');
-            return false;
             user.warnCounters += 1;
-                room.add('|html|<font color="#FF00BF">' + user.name + ' was warned by ' + '<i><b>' + bot.name + '</b>(caps)</i></font>');
-                user.send('|c|~|/warn caps');
+            room.add('|html|<font color="#FF00BF">' + user.name + ' was warned by ' + '<i><b>' + bot.name + '</b>(caps)</i></font>');
+            user.send('|c|~|/warn caps');
         }
+    }
+    if (user.warnCounters > 4) {
+        room.add('|html|<font color="#FF00BF">' + user.name + ' was muted by ' + '<i><b>' + bot.name + '</b>(more than 4 warnings)</i></font>');
+        user.mute(room.id, 60 * 60 * 1000, true);
+        return false;
+    }
+    if (message === message.toUpperCase() && message.length >= 11) {
+        room.add('|c|' + user.name + '|' + message);
+        user.warnCounters += 1;
+        room.add('|html|<font color="#FF00BF">' + user.name + ' was warned by ' + '<i><b>' + bot.name + '</b>(caps)</i></font>');
+        user.send('|c|~|/warn caps');
+        return false;
+    }
+    if (spamroom[user.userid]) {
+        Rooms.rooms.spamroom.add('|c|' + user.getIdentity() + '|' + message);
+        connection.sendTo(room, "|c|" + user.getIdentity() + "|" + message);
+        return false;
+    }
+    if (message.toLowerCase().indexOf(".psim") > -1) {
+        connection.sendTo(room, '|raw|<strong class=\"message-throttle-notice\">Advertising is not allowed please do not.</strong>');
+        return false;
+        user.warnCounters += 1;
+        room.add('|html|<font color="#FF00BF">' + user.name + ' was warned by ' + '<i><b>' + bot.name + '</b>(caps)</i></font>');
+        user.send('|c|~|/warn caps');
+    }
 
-        if (message.toLowerCase().indexOf("play.pokemonshowdown.com") > -1) {
-            connection.sendTo(room, '|raw|<strong class=\"message-throttle-notice\">Advertising is not allowed please do not.</strong>');
-            return false;
-            user.warnCounters += 1;
-                room.add('|html|<font color="#FF00BF">' + user.name + ' was warned by ' + '<i><b>' + bot.name + '</b>(caps)</i></font>');
-                user.send('|c|~|/warn caps');
-        }
+    if (message.toLowerCase().indexOf("play.pokemonshowdown.com") > -1) {
+        connection.sendTo(room, '|raw|<strong class=\"message-throttle-notice\">Advertising is not allowed please do not.</strong>');
+        return false;
+        user.warnCounters += 1;
+        room.add('|html|<font color="#FF00BF">' + user.name + ' was warned by ' + '<i><b>' + bot.name + '</b>(caps)</i></font>');
+        user.send('|c|~|/warn caps');
+    }
 
-        if (message.toLowerCase().indexOf("psim") > -1) {
-            connection.sendTo(room, '|raw|<strong class=\"message-throttle-notice\">Advertising is not allowed please do not.</strong>');
-            return false;
-            user.warnCounters += 1;
-                room.add('|html|<font color="#FF00BF">' + user.name + ' was warned by ' + '<i><b>' + bot.name + '</b>(caps)</i></font>');
-                user.send('|c|~|/warn caps');
-        }
-        if (message.toLowerCase().indexOf("ps im") > -1) {
-            connection.sendTo(room, '|raw|<strong class=\"message-throttle-notice\">Advertising is not allowed please do not.</strong>');
-            return false;
-            user.warnCounters += 1;
-                room.add('|html|<font color="#FF00BF">' + user.name + ' was warned by ' + '<i><b>' + bot.name + '</b>(caps)</i></font>');
-                user.send('|c|~|/warn caps');
-        }
-        if (message.toLowerCase().indexOf("psi m") > -1) {
-            connection.sendTo(room, '|raw|<strong class=\"message-throttle-notice\">Advertising is not allowed please do not.</strong>');
-            return false;
-            user.warnCounters += 1;
-                room.add('|html|<font color="#FF00BF">' + user.name + ' was warned by ' + '<i><b>' + bot.name + '</b>(caps)</i></font>');
-                user.send('|c|~|/warn caps');
-        }
-        if (message.toLowerCase().indexOf("p sim") > -1) {
-            connection.sendTo(room, '|raw|<strong class=\"message-throttle-notice\">Advertising is not allowed please do not.</strong>');
-            return false;
-        }
-        if (message.toLowerCase().indexOf(".prism") > -1) {
-            connection.sendTo(room, '|raw|<strong class=\"message-throttle-notice\">Advertising is not allowed please do not.</strong>');
-            return false;
-            user.warnCounters += 1;
-                room.add('|html|<font color="#FF00BF">' + user.name + ' was warned by ' + '<i><b>' + bot.name + '</b>(caps)</i></font>');
-                user.send('|c|~|/warn caps');
-        }
-    };
+    if (message.toLowerCase().indexOf("psim") > -1) {
+        connection.sendTo(room, '|raw|<strong class=\"message-throttle-notice\">Advertising is not allowed please do not.</strong>');
+        return false;
+        user.warnCounters += 1;
+        room.add('|html|<font color="#FF00BF">' + user.name + ' was warned by ' + '<i><b>' + bot.name + '</b>(caps)</i></font>');
+        user.send('|c|~|/warn caps');
+    }
+    if (message.toLowerCase().indexOf("ps im") > -1) {
+        connection.sendTo(room, '|raw|<strong class=\"message-throttle-notice\">Advertising is not allowed please do not.</strong>');
+        return false;
+        user.warnCounters += 1;
+        room.add('|html|<font color="#FF00BF">' + user.name + ' was warned by ' + '<i><b>' + bot.name + '</b>(caps)</i></font>');
+        user.send('|c|~|/warn caps');
+    }
+    if (message.toLowerCase().indexOf("psi m") > -1) {
+        connection.sendTo(room, '|raw|<strong class=\"message-throttle-notice\">Advertising is not allowed please do not.</strong>');
+        return false;
+        user.warnCounters += 1;
+        room.add('|html|<font color="#FF00BF">' + user.name + ' was warned by ' + '<i><b>' + bot.name + '</b>(caps)</i></font>');
+        user.send('|c|~|/warn caps');
+    }
+    if (message.toLowerCase().indexOf("p sim") > -1) {
+        connection.sendTo(room, '|raw|<strong class=\"message-throttle-notice\">Advertising is not allowed please do not.</strong>');
+        return false;
+    }
+    if (message.toLowerCase().indexOf(".prism") > -1) {
+        connection.sendTo(room, '|raw|<strong class=\"message-throttle-notice\">Advertising is not allowed please do not.</strong>');
+        return false;
+        user.warnCounters += 1;
+        room.add('|html|<font color="#FF00BF">' + user.name + ' was warned by ' + '<i><b>' + bot.name + '</b>(caps)</i></font>');
+        user.send('|c|~|/warn caps');
+    }
+};
+exports.CheckBannedWords = CheckBannedWords;
+exports.CheckBannedSites = CheckBannedSites;
