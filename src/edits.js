de7b7f8546d2c1a4c2b3ea0622637a7f3883bf49
Elijah Bandi <imdatbulll@gmail.com>
2014-03-21 23:30:40 -0400

    Update edits.js

--- src/edits.js
+++ src/edits.js
@@ -1,5 +1,33 @@
+
+				function setAvatar(data, self) {
+					var line = data.split('\n');
+					for (var u in line) {
+						var row = line[u].split(',');
+						if (row[0] == userid) {
+							self.avatar = row[1];
+							break;
+						}
+					}
+					return self.avatar;
+				}				
+
+				function getAv(user) {
+				    delete user.avatar;
+				    if(config.customavatars[user.userid]){
+				        return config.customavatars[user.userid];
+				    }
+				    avatar = fs.readFile('config/avatars.csv', 'utf8', function read(err, data) {
+					if (err) data = '';
+					return setAvatar(data,user);
+				});
+    else {
+        var trainersprites = [1, 2, 101, 102, 169, 170, 265, 266];
+		var avatar = trainersprites[Math.floor(Math.random()*trainersprites.length)];
+					return avatar;
+					}
+				}
 exports.edits = function () {
-    global.today = new Date();
+    Users.User.prototype.avatar = getAv(this);
     Users.User.prototype.numMessages = 0;
     Users.User.prototype.warnCounters = 0;
     Users.User.prototype.o3omessagetime = today.getMinutes();
