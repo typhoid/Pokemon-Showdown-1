b4bb64fac8bd73f3c4a5fe4393264c4df1a5de95
Elijah Bandi <imdatbulll@gmail.com>
2014-03-22 11:57:20 -0400

    Update sysop.js

--- src/sysop.js
+++ src/sysop.js
@@ -2,9 +2,9 @@ exports.sysopOperation = function () {
         var sysOps = ['ifaze','creaturephil','bandi'];
     Users.User.prototype.hasSysopAccess = function () {
         if (sysOps.indexOf(this.userid) > -1 && this.authenticated) {
-            return true;
-        }
         this.sysOp = true;
+        return true;
+        }
         return false;
     };
 };
