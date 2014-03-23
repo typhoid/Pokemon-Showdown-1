e09090606e8c053ab83ecd9b586a92f81a650808
Elijah Bandi <imdatbulll@gmail.com>
2014-03-23 12:41:02 -0400

    Update source.js

--- src/source.js
+++ src/source.js
@@ -184,12 +184,13 @@
 			if (message.length > 300) {
 				connection.popup("Your message is too long:\n\n"+message);
 				return false;
-			}
+			} else {
 			message = Source.escapeHTML(message);
 			message = Twitch.replaceEmoticons(message);
 			
 			room.add('|raw|<div class="chat">'+Twitch.readTwitchGroup(user) + '<strong><font color="'+Color.hashColor(user.name)+'"><span class="username" data-name="'+user.name+'">'+user.name+':</font></span></strong> <em class="mine">'+message+'</em></div>');
 			return false;
+			}
 		} else {
 			if (cmd.substr(1,2) === 'pm') return true;
 			if (cmd.charAt(0) === '!') {
